e20ef74ee703d4646208e5125c767a61b105300f
Calvin <calvin.chan.h@gmail.com>
2014-04-04 22:08:43 -0700

    Requests longer-lasting access token and checks if stored access tokens are still valid

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,7 @@
 var Mongoose = require('mongoose');
+var uuid = require('node-uuid');
+var graph = require('fbgraph');
+
 var db = Mongoose.connection;
 db.on('error',console.error);
 db.once('open', function() {
@@ -11,11 +14,25 @@ var mongoUri = process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 Mongoose.connect(mongoUri);
 
 var userModelSchema = new Mongo.Schema({
+    fbId : {
+        type : String,
+        required : true,
+        unique: true
+    },
     apiKey : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    accessToken : {
         type : String,
         required : true
     },
-    accessToken : {
+    expiresAt : {
+        type : Number,
+        required : true
+    },
+    machineId : {
         type : String,
         required : true
     }
@@ -32,7 +49,60 @@ exports.getAccessToken = function(apiKey, callback) {
             console.log(err);
             return;
         } else {
-            callback(user.accessToken);
+            // Checks to see if stored access token is still valid
+            if (Date.now()/1000 > user.expiresAt) {
+                // Requests new access token
+                graph.extendAccessToken({
+                    "access_token": user.accessToken,
+                    "client_id": process.env.FB_CLIENT_ID,
+                    "client_secret": process.env.FB_CLIENT_SECRET 
+                }, function(err, res) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    user.accessToken = res.access_token;
+                    user.expiresAt = Date.now()/1000 + res.expires_in;     
+                    // Updates user with new access token
+                    user.save(function(err, user){
+                        if (err) {
+                            console.log(err);
+                            return;
+                        } else {
+                            callback(user.accessToken);
+                        }
+                    })
+                });
+            } else {
+                callback(user.accessToken);
+            }
         }
     });
-}
+};
+
+exports.createNewUser = function(fbId, accessToken, callback) {
+    graph.setAccessToken(access_token); 
+    graph.extendAccessToken({
+        "access_token": accessToken,
+        "client_id": process.env.FB_CLIENT_ID,
+        "client_secret": process.env.FB_CLIENT_SECRET 
+    }, function(err, res){
+        var longAccessToken = res.access_token;
+        var expiresAt = Date.now()/1000 + res.expires_in;
+        var user = new users({
+            fbId : fbId,
+            apiKey : uuid.v1(),
+            accessToken : longAccessToken,
+            expiresAt : expiresAt,
+            machineId : res.machine_id
+        });
+        user.save(function(err, user) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                callback(user);
+            }
+        });
+    });
+};
