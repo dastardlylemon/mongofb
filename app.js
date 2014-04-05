4af28059cffcd208be90f2ce41a06e90cd5af572
Calvin <calvin.chan.h@gmail.com>
2014-04-04 19:40:29 -0700

    Added routes

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.post('/query', routes.query);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+var queryController = require("../controllers/queryController");
+var mongoController = require("../controllers/mongoController");
+var facebookController = require("../controllers/facebookController");
+
+exports.query = function(req,res) {
+    var query = req.params.query;
+    var apiKey = req.params.key;
+    
+}
