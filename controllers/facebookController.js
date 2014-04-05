ca176ff667f8d84ce2e7df5ecbfa6f881a2e0f48
Calvin <calvin.chan.h@gmail.com>
2014-04-05 11:25:29 -0700

    Fixed find

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -103,6 +103,8 @@ function matchesQuery(query, commentObj) {
         if (message.hasOwnProperty(i)) {
             if (message[i].search(queryObj[i]) !== -1) {
                 continue;
+            } else {
+                return false;
             }
         } else {
             return false;
