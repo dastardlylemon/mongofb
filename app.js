5fafb5414863be30d2de427c0d2d4578683c4766
Daniel Duan <danielduan88@yahoo.com>
2014-04-04 19:14:23 -0700

    setting up

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+var Mongoose = require('mongoose');
+var mongoUri = process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+var db = Mongoose.createConnection(mongoUri);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(allowCrossDomain);
+app.use(app.router);
+app.use(express.limit('50mb'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
